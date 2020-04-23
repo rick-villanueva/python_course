734f3c822076e6d555599b80fb3a673b1b7f7a56
Rick Villanueva <villanuevap.rick@gmail.com>
2020-04-23 15:52:48 -0600

    last_classes

--- Code/object_oriented_prog.py
+++ Code/object_oriented_prog.py
@@ -196,3 +196,47 @@ a = Book('Paradiso','Dante',800)
 
 del a #deletes from memory
 
+#Distance and slope
+class Line:
+    
+    def __init__(self,coor1,coor2):
+        self.coor1 = coor1
+        self.coor2 = coor2
+    
+    def distance(self):
+        x1,y1 = self.coor1
+        x2,y2 = self.coor2
+        return ((x2-x1)**2 + (y2-y1)**2)**0.5
+    
+    def slope(self):
+        x1,y1 = self.coor1
+        x2,y2 = self.coor2
+        return (y2-y1)/(x2-x1)
+    
+coordinate1 = (7,4)
+coordinate2 = (8,10)
+
+li = Line(coordinate1,coordinate2)
+
+li.distance()
+li.slope()
+
+#Cylinder formulas
+class Cylinder:
+    
+    pi = 3.1415
+
+    def __init__(self,height=1,radius=1):
+        self.height = height
+        self.radius = radius
+        
+    def volume(self):
+        return (Cylinder.pi*(self.radius**2)*self.height)
+    
+    def surface_area(self):
+        return ((2*Cylinder.pi*self.radius*self.height)+(2*Cylinder.pi*(self.radius**2)))
+
+cylinder = Cylinder(2,3)
+
+cylinder.volume()
+cylinder.surface_area()
