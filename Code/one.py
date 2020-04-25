5be9a702da975f0a30f6c75cb242c439681d4e5b
Rick Villanueva <villanuevap.rick@gmail.com>
2020-04-24 19:53:21 -0600

    packages

---
+++ Code/one.py
@@ -0,0 +1,12 @@
+#one.py
+def func():
+    print('Function in one.py')
+
+print('Top level in one.py')
+
+if __name__ == '__main__':
+    print('one.py is being run directly')
+else: 
+    print('one.py has been imported')
+    
+#usually includes the logic of a script 
---
+++ Code/program.py
@@ -0,0 +1,6 @@
+from main_package import main_script
+from main_package.sub_package import sub_script
+
+main_script.report_main()
+
+sub_script.sub_report()
